2548746da37560ef4f2b5f3f5b3acd80169279b3
kundan <kundankumar28884@gmail.com>
2024-09-10 11:56:39 +0530

    attemp 2nd

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,6 @@
-"use client";
 import DragPlate from "@/components/drag-plate/DragPlate";
 
-export default function Home({ className }: { className: string }) {
+export default function Home() {
   return (
     <div className="absolute w-full h-screen top-0 left-0 flex items-center justify-center">
       <DragPlate className="bg-blue-800/20 outline outline-1 outline-blue-600/60" />
